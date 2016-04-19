775ddc7e153754bf420a46346bce633789a03823
Daniel Huhn <daniel@danielhuhn.de>
2016-04-20 00:29:50 +0200

    added script to read barcode scanner input

---
+++ readinput.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from printNumber import print_fd
+
+device = '/dev/input/by-id/usb-GASIA_PS2toUSB_Adapter-event-kbd'
+fp = open(device, 'rb')
+
+counter = 0
+i = 0
+while True:
+   buffer = fp.read(8)
+   for c in buffer:
+      if ord(c) > 0:
+         print ord(c)
+   i = i + 1
+
+   if i % 270 == 0:
+       counter += 1
+       print_fd(counter)
